7d62b4c6b45ff1741acd08f2adc736123fbc2b11
Riajul Islam <riajulpislam@gmail.com>
2025-01-14 23:41:20 +0600

    project-setup-and-customization

---
+++ README.md
@@ -0,0 +1,6 @@
+project---------
+
+1-> project setup using vite
+2-> tailwind install and setup
+3-> project customization
+4->
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+
+const App = () => {
+  return (
+    <h1 className="font-primary">Hi developers</h1>
+  )
+}
+
+export default App
