aec6a4227e8715cba3f852658f10811a5d02e7b4
Riajul Islam <riajulpislam@gmail.com>
2025-01-20 10:47:45 +0600

    filter-product

--- src/pages/shop/Products.jsx
+++ src/pages/shop/Products.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react"
 import { products } from "../../utils/products"
 import ProductCard from "./ProductCard"
-
+import btnIcon from "../../assets/button-icon.png"
 
 const Products = ({hedline}) => {
     const categories = ["Chair","Beds","Sofa","Lamp",]
+     const [selectedCategory,setSelectedCategory] = useState("Chair");
+     const filteredProducts = products.filter((product)=> product.category === selectedCategory)
+     const [visibleProducts, setVisibleProducts] = useState(4);
+
+     const loadMoreProducts = ()=> {
+      setVisibleProducts((prev)=> prev + 4)
+     }
+    
   return (
     <div>
         <div className="section-container">
@@ -14,7 +23,10 @@ const Products = ({hedline}) => {
               <div className="flex flex-col sm:flex-row items-center md:justify-between justify-center gap-4">
                  {
                     categories.map((category)=> (
-                        <button key={category} className={`py-1.5 sm:px-5 px-8 rounded-full hover:bg-primary hover:text-white transition-colors `}>{category}</button>
+                        <button onClick={()=> {
+                          setSelectedCategory(category)
+                          setVisibleProducts(4)
+                        }} key={category} className={`py-1.5 sm:px-5 px-8 rounded-full hover:bg-primary hover:text-white transition-colors ${selectedCategory === category ? "bg-white text-primary" : "text-secondary"} `}>{category}</button>
                     ))
                  }
               </div>
@@ -23,11 +35,23 @@ const Products = ({hedline}) => {
             {/* product grid  */}
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
                 {
-                    products.map((product,index)=> (
+                    filteredProducts.slice(0,visibleProducts).map((product,index)=> (
                      <ProductCard key={index} product={product}/>
                     ))
                 }
             </div>
+            {/* load mor btn */}
+            {
+              visibleProducts < filteredProducts.length && (
+                <div className="flex justify-center items-center mt-8">
+                   <button
+                   onClick={loadMoreProducts}
+                    className="text-base font-semibold text-primary flex items-center gap-1"> View All
+                      <img src={btnIcon} alt="btn icon" />
+                    </button>
+                </div>
+              )
+            }
         </div>
     </div>
   )
--- src/utils/products.js
+++ src/utils/products.js
@@ -40,7 +40,8 @@ export const products = [
     imageUrl: "chair-5.png",
     rating: 4.5,
   },
-
+ 
+  
   // Beds
   {
     id: 6,
