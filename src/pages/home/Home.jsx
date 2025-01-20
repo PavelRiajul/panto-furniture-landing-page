d0d1577b5ecb76930a62077d272c9c5a5320919a
Riajul Islam <riajulpislam@gmail.com>
2025-01-20 11:29:07 +0600

    materials-section-complete

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import Products from "../shop/Products"
 import Expriences from "./Expriences"
 import Hero from "./Hero"
+import Materials from "./Materials"
 import WhyChoose from "./WhyChoose"
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
      <WhyChoose/>
      <Products hedline="Best Selling Product"/>
      <Expriences/>
+     <Materials/>
     </div>
   )
 }
